efc7d17531456210a1e195b64a01a0aca65ec679
Macflash <the.alex.breyfogle@gmail.com>
2020-10-20 23:14:57 -0700

    Updates

---
+++ precache-manifest.026c8cc286245e724925af34112ba758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fe4e42998e8df3558a910bb96b70b8",
+    "url": "/murder-on-the-hill/index.html"
+  },
+  {
+    "revision": "61223955d71e3c7725a1",
+    "url": "/murder-on-the-hill/static/css/main.14dad1a1.chunk.css"
+  },
+  {
+    "revision": "cee9ad54313100bf95ab",
+    "url": "/murder-on-the-hill/static/js/2.1471b5d7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/murder-on-the-hill/static/js/2.1471b5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61223955d71e3c7725a1",
+    "url": "/murder-on-the-hill/static/js/main.0b329b7d.chunk.js"
+  },
+  {
+    "revision": "68add5ba027b449fcdef",
+    "url": "/murder-on-the-hill/static/js/runtime-main.0ef4b842.js"
+  }
+]);
