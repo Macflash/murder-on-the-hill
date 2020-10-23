9b89f9cb73b4390ff2e64b011ba49b5d16a74331
Macflash <the.alex.breyfogle@gmail.com>
2020-10-23 00:08:00 -0700

    Updates

--- precache-manifest.68c4aa3c0d7b5151acf167d72f4ea33f.js
+++ precache-manifest.68c4aa3c0d7b5151acf167d72f4ea33f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a7d20197465d7136cc6198809065d14",
+    "revision": "569b99e2e4ab76c13d2132ed877e175f",
     "url": "/murder-on-the-hill/index.html"
   },
   {
-    "revision": "d8602647e7562eb8e2b4",
+    "revision": "b59c5b269211c598aa48",
     "url": "/murder-on-the-hill/static/css/main.2cf0a9eb.chunk.css"
   },
   {
-    "revision": "cee9ad54313100bf95ab",
-    "url": "/murder-on-the-hill/static/js/2.1471b5d7.chunk.js"
+    "revision": "7b29368a704e1a658efd",
+    "url": "/murder-on-the-hill/static/js/2.fee7fb8f.chunk.js"
   },
   {
     "revision": "65075b20798fa24012a1ad453c78c90d",
-    "url": "/murder-on-the-hill/static/js/2.1471b5d7.chunk.js.LICENSE.txt"
+    "url": "/murder-on-the-hill/static/js/2.fee7fb8f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8602647e7562eb8e2b4",
-    "url": "/murder-on-the-hill/static/js/main.14957f19.chunk.js"
+    "revision": "b59c5b269211c598aa48",
+    "url": "/murder-on-the-hill/static/js/main.4253dc86.chunk.js"
   },
   {
     "revision": "68add5ba027b449fcdef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/murder-on-the-hill/precache-manifest.c7941efceea8bd3e546ea5537a7030e1.js"
+  "/murder-on-the-hill/precache-manifest.68c4aa3c0d7b5151acf167d72f4ea33f.js"
 );
 
 self.addEventListener('message', (event) => {
